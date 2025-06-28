0c7f5b936369dfb46132f365c19edf7f19c1d875
wslong <68840863+wslong20@users.noreply.github.com>
2025-06-28 19:22:48 +0800

    Update gen_conversation_test_gsm8k.sh

--- PI/scripts/test/gen_conversation_test_gsm8k.sh
+++ PI/scripts/test/gen_conversation_test_gsm8k.sh
@@ -1,5 +1,5 @@
-python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.2 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train --dataset gsm8k
-python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.4 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train --dataset gsm8k
-python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.6 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train --dataset gsm8k
-python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.8 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train --dataset gsm8k
-python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 1.0 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train --dataset gsm8k
+python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.2 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test --dataset gsm8k
+python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.4 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test --dataset gsm8k
+python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.6 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test --dataset gsm8k
+python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 0.8 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test --dataset gsm8k
+python gen_graph_for_gsm8k.py --num_nodes 8 --sparsity 1.0 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test --dataset gsm8k
