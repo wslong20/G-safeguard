77380de445862a997337d7f81527a7d17125b1ab
wslong <68840863+wslong20@users.noreply.github.com>
2025-06-28 19:24:04 +0800

    Update gen_conversation_test.sh

--- TA/scripts/test/gen_conversation_test.sh
+++ TA/scripts/test/gen_conversation_test.sh
@@ -1,5 +1,5 @@
-python gen_graph.py --num_nodes 8 --sparsity 0.2 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train
-python gen_graph.py --num_nodes 8 --sparsity 0.4 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train
-python gen_graph.py --num_nodes 8 --sparsity 0.6 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train
-python gen_graph.py --num_nodes 8 --sparsity 0.8 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train
-python gen_graph.py --num_nodes 8 --sparsity 1.0 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase train
+python gen_graph.py --num_nodes 8 --sparsity 0.2 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test
+python gen_graph.py --num_nodes 8 --sparsity 0.4 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test
+python gen_graph.py --num_nodes 8 --sparsity 0.6 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test
+python gen_graph.py --num_nodes 8 --sparsity 0.8 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test
+python gen_graph.py --num_nodes 8 --sparsity 1.0 --num_graphs 20 --num_attackers 3 --samples 12 --model_type gpt-4o-mini --phase test
